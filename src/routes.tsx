50bbc018a1fb3aa6c0cb9c782e8f8d9d5680e92e
lcsAndrade <lucascandradex@protonmail.com>
2024-04-21 22:51:45 -0300

    feat: authenticate validation

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,14 +1,26 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 
+import Home from './pages/Home';
 import Login from './pages/Login';
 import Navigation from './components/Navigation';
+import { useAppContext } from './context';
+import Loading from './components/Loading';
 
 const AppRoutes = () => {
+  const { user } = useAppContext();
+
+  if (user === undefined) {
+    return <Loading />;
+  };
+
+  const isAuthenticated: boolean = !!user;
+
   return (
     <div>
       <Navigation />
       <Routes>
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={isAuthenticated ? <Home /> : <Navigate to="login" replace />} />
+        <Route path="/login" element={isAuthenticated ? <Navigate to="/" replace /> : <Login />} />
         <Route path="*" element={<Navigate to="/login" />} />
       </Routes>
     </div>
