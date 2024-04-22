5dba518d61f78497e9193cca5659e15f5bf02693
lcsAndrade <lucascandradex@protonmail.com>
2024-04-21 23:28:42 -0300

    feat: add user data to header

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,13 +1,25 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBuildingColumns } from '@fortawesome/free-solid-svg-icons';
+import { faBuildingColumns, faUser } from '@fortawesome/free-solid-svg-icons';
+import { useAppContext } from '../../context';
 
 const Navigation = () => {
+  const { user } = useAppContext();
+
   return (
-    <div className='flex bg-stone-400 py-4 px-4'>
+    <div className='flex bg-stone-400 py-4 px-4 items-center justify-between'>
       <div className='flex items-center gap-2'>
         <FontAwesomeIcon icon={faBuildingColumns} size='2x' />
         <h1 className='text-xl font-bold'>MyBank</h1>
       </div>
+      {user && (
+        <div>
+          <FontAwesomeIcon icon={faUser} size="xl" />
+          <span className='ml-2 text-md font-bold'>
+            {user.name}
+          </span>
+
+        </div>
+      )}
     </div>
   )
 };
