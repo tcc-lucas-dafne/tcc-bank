f0f9416a7f650fd48641744705e2ee51c0626a3a
lcsAndrade <lucascandradex@protonmail.com>
2024-06-16 02:30:05 -0300

    fix: adiciona toast container

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import './App.css';
 import AppRoutes from './routes';
 import AppProvider from './context';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <div className="App">
       <AppProvider>
         <AppRoutes />
+        <ToastContainer />
       </AppProvider>
     </div>
   );
